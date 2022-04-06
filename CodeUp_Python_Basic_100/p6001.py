5d4681ffb2848456b692a75c3ac56d31775539eb
jp0301 <jp0301@naver.com>
2022-04-06 18:35:15 +0900

    CodeUp Basic 22-04-06

---
+++ CodeUp_Python_Basic_100/p6001.py
@@ -0,0 +1,24 @@
+# [기초-출력] 출력하기01
+print("Hello")
+
+# [기초-출력] 출력하기02
+print("Hello World")
+
+# [기초-출력] 출력하기03
+print("Hello")
+print("World")
+
+# [기초-출력] 출력하기04
+print("'Hello'")
+
+# [기초-출력] 출력하기05
+print('"Hello World"')
+
+# [기초-출력] 출력하기06
+print("\"!@#$%^&*()\'")
+
+# [기초-출력] 출력하기07
+print("\"C:\Download\\'hello\'.py\"")
+
+# [기초-출력] 출력하기08
+print("print(\"Hello\\nWorld\")")
---
+++ CodeUp_Python_Basic_100/p6009.py
@@ -0,0 +1,48 @@
+# 6009. [기초-입출력] 문자 1개 입력받아 그대로 출력하기
+c = input()
+print(c)
+
+# 6010. [기초-입출력] 정수 1개 입력받아 int로 변환하여 출력하기
+a = input()
+a = int(a)
+print(a)
+
+# 6011. [기초-입출력] 실수 1개 입력받아 변환하여 출력하기
+f = input()
+f = float(f)
+print(f)
+
+# 6012. [기초-입출력] 정수 2개 입력받아 그대로 출력하기
+a = input()
+b = input()
+print(a)
+print(b)
+
+# 6013. [기초-입출력] 문자 2개 입력받아 순서 바꿔 출력하기
+a = input()
+b = input()
+print(b)
+print(a)
+
+# 6014. [기초-입출력] 실수 1개 입력받아 3번 출력하기
+f = float(input())
+print(f)
+print(f)
+print(f)
+
+# 6015. [기초-입출력] 정수 2개 입력받아 그대로 출력하기
+a, b = input().split()
+print(a)
+print(b)
+
+# 6016. [기초-입출력] 문자 2개 입력받아 순서 바꿔 출력하기2
+a, b = input().split()
+print(b, a)
+
+# 6017. [기초-입출력] 실수 1개 입력받아 3번 출력하기
+s = input()
+print(s,s,s)
+
+# 6018. [기초-입출력] 시간 입력받아 그대로 출력하기
+a, b = input().split(':')
+print(a, b, sep=':')
