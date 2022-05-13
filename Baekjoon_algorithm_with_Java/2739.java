6085b0be15103a317a06c51a870a70e714c01fb2
Wonseok Park <jp0301@naver.com>
2022-05-13 19:47:58 +0900

    Create 2739.java

---
+++ Baekjoon_algorithm_with_Java/2739.java
@@ -0,0 +1,22 @@
+/*
+* 반복문 #1
+* 2739번
+* 구구단 출력하기
+*/
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = 1; i < 10; i++) {
+			System.out.println( n + " * " + i + " = " + (n*i) );
+		}
+		
+		
+	}
+}
