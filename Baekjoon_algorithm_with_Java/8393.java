ab733dc7e620c1360e77a3456b700e83224bd53d
Wonseok Park <jp0301@naver.com>
2022-05-13 20:12:40 +0900

    Create 8393.java

---
+++ Baekjoon_algorithm_with_Java/8393.java
@@ -0,0 +1,23 @@
+/*
+* 반복문 #3
+* 8393번
+* 합
+*/
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        
+        for(int i = 1; i <= n; i++) {
+        	a += i;
+        }
+        System.out.println(a);
+		
+		
+	}
+}
