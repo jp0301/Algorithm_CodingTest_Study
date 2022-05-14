1297ad1e253fb18ac7610f3c92b7e467f3d600ea
Wonseok Park <jp0301@naver.com>
2022-05-14 12:20:45 +0900

    Create 2742.java

---
+++ Baekjoon_algorithm_with_Java/반복문/2742.java
@@ -0,0 +1,21 @@
+/*
+* 반복문 #6
+* 2742번
+* 기찍 N
+*/
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = n; i >= 1; i--) {
+			System.out.println(i);
+		}
+       
+		
+	}
+}
