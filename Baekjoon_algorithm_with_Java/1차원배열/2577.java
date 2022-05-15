1dc11df64ec05751180542ad3d58c725b9195bcd
Onedoli <jp0301@naver.com>
2022-05-15 12:41:09 +0900

    Update 2577.java

--- Baekjoon_algorithm_with_Java/1차원배열/2577.java
+++ Baekjoon_algorithm_with_Java/1차원배열/2577.java
@@ -13,15 +13,18 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		 
 		int[] arr = new int[10];
- 
+		
+		//val에다가 br.readLine으로 읽은 값을 곱해서 저장한다.
 		int val = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
- 
+		
+		// val의 int형을 string형으로 변환해준 뒤
 		String str = String.valueOf(val);
- 
+		
+		//for문을 통해 해당 문자열의 문자 값 - 48(또는 -'0')을 추출해 내 int 배열의 index값을 1증가 시킨다.
 		for (int i = 0; i < str.length(); i++) {
 			arr[(str.charAt(i) - 48)]++;
 		}
- 
+		
 		for (int v : arr) {
 			System.out.println(v);
 		}
